457489147e392622f5a4166bdc539c7668215352
Rasmus Voss <rasmus.voss@keylane.com>
2018-03-13 17:47:17 +0100

    Ny funktion til blockbreake

--- blockBreaker.js
+++ blockBreaker.js
@@ -1,10 +1,13 @@
 
+var dummy = ""
 var score = {};
 var vinderscore = 10;
 var erDerEnvinder = "NEJ"
 var blockType = "GRASS"
+var erSpilletStartet = "NEJ"
 events.blockBreak( harDuVundet );
 events.entityDeath( noget );
+
 function noget( event ){
   var utils = require('utils');
   players = utils.players();
@@ -14,21 +17,29 @@ function noget( event ){
     }
 }
 
-
 function harDuVundet( event ){
-    if (score[event.player.name] === undefined) {
-      score[event.player.name] = 0
+    if (erSpilletStartet == "JA") {
+      if (score[event.player.name] === undefined) {
+        score[event.player.name] = 0
+      }
+      if (event.block.type == blockType) {
+        score[event.player.name] = score[event.player.name] - 1
+      } else {
+        score[event.player.name] = score[event.player.name] + 1
+      }
+      if (score[event.player.name] == vinderscore){
+        echo (event.player, "Du er bare en vinder")
+        erSpilletStartet = "NEJ"
+        score[event.player.name] = 0
+      } else {
+        echo (event.player, event.block.type)
+      }
+      echo (event.player, score[event.player.name])
     }
-    if (event.block.type == blockType) {
-      score[event.player.name] = score[event.player.name] - 1
-    } else {
-      score[event.player.name] = score[event.player.name] + 1
-    }
-    if (score[event.player.name] == vinderscore){
-      echo (event.player, "Du er bare en vinder")
-      score[event.player.name] = 0
-    } else {
-      echo (event.player, event.block.type)
-    }
-    echo (event.player, score[event.player.name])
 };
+
+function startSpil(params, sender) {
+  erSpilletStartet = "JA"
+}
+
+command(startSpil, dummy);
